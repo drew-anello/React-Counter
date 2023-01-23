de29bada0d60cad290c4eeaa7bf609b0eccaff83
Drew Anello <drew.anello@gmail.com>
2023-01-22 20:15:42 -0500

    adds increase and decrease button app is functioning

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,6 +5,8 @@ function Counter() {
     return (
     <div>
         <h2>The Count is</h2> <h1>{count}</h1>
+        <button onClick={() =>setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count -1)}>Decrement</button>
     </div>
   )
 }
