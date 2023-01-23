94d06a0650b9b2f176060ed9912dd2db6e43e4d1
Drew Anello <drew.anello@gmail.com>
2023-01-22 20:56:31 -0500

    adds scss styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+// import './App.css'
+import './App.scss'
 import React from "react" 
 import Counter from './components/Counter'
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,8 +5,14 @@ function Counter() {
     return (
     <div>
         <h2>The Count is</h2> <h1>{count}</h1>
-        <button onClick={() =>setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count -1)}>Decrement</button>
+         <div className='button-container'>
+    
+             <button onClick={() =>setCount(count + 1)}>Increment</button>
+
+             <button onClick={() => setCount(count -1)}>Decrement</button>
+   
+         </div>
+
     </div>
   )
 }
